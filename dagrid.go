37c675b7c77fad1c86573c0f7c5f639b71d0e658
Ingrid Rebecca Abraham <ingridra@met.no>
2022-07-06 13:32:25 +0200

    pass node contents into functions instead of nodes

--- dagrid.go
+++ dagrid.go
@@ -17,22 +17,30 @@ func set_insert(set map[int]struct{}, elem int) {
 	set[elem] = struct{}{}
 }
 
-func new_dag(starting_node node) dag {
-	starting_node.index = 0
-	return dag{
-		roots:  map[int]struct{}{0: {}},
-		leaves: map[int]struct{}{0: {}},
-		nodes:  []node{starting_node},
+func new_node(contents string, index int) node {
+	return node{
+		contents: contents,
+		index:    index,
+		children: make(map[int]struct{}),
+		parents:  make(map[int]struct{}),
 	}
 }
 
-func (dag dag) insert_child(child node, parent node) {
-	child.index = len(dag.nodes)
+func new_dag(starting_node_contents string) dag {
+	return dag{
+		roots:  map[int]struct{}{0: {}},
+		leaves: map[int]struct{}{0: {}},
+		nodes:  []node{new_node(starting_node_contents, 0)},
+	}
+}
+
+func (dag dag) insert_child(child_contents string, parent_index int) {
+	child := new_node(child_contents, len(dag.nodes))
 
 	dag.nodes = append(dag.nodes, child)
 	set_insert(dag.leaves, child.index)
-	delete(dag.leaves, parent.index)
+	delete(dag.leaves, parent_index)
 
-	set_insert(child.parents, parent.index)
-	set_insert(parent.children, child.index)
+	set_insert(child.parents, parent_index)
+	set_insert(dag.nodes[parent_index].children, child.index)
 }
