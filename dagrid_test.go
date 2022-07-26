d3db6a07a53a7e37a024a0a4093943dab7eca156
Ingrid <git@ingrids.email>
2022-07-26 13:17:47 +0200

    update tests to match the exports

--- dagrid_test.go
+++ dagrid_test.go
@@ -3,27 +3,27 @@ package dagrid
 import "testing"
 
 func TestTransitiveReduce(t *testing.T) {
-	dag := new_dag()
+	dag := New_dag()
 
-	a := dag.insert_free_node("a")
+	a := dag.Insert_free_node("a")
 
-	b := dag.insert_child(a, "b")
-	c := dag.insert_child(a, "c")
-	d := dag.insert_child(a, "d")
-	e := dag.insert_child(a, "e")
+	b := dag.Insert_child(a, "b")
+	c := dag.Insert_child(a, "c")
+	d := dag.Insert_child(a, "d")
+	e := dag.Insert_child(a, "e")
 
-	dag.add_edge(b, d)
-	dag.add_edge(c, d)
-	dag.add_edge(c, e)
-	dag.add_edge(d, e)
+	dag.Add_edge(b, d)
+	dag.Add_edge(c, d)
+	dag.Add_edge(c, e)
+	dag.Add_edge(d, e)
 
-	if dag.count_edges() != 8 {
-		t.Errorf("dag has %v edges, expected 8 edges", dag.count_edges())
+	if dag.Count_edges() != 8 {
+		t.Errorf("dag has %v edges, expected 8 edges", dag.Count_edges())
 	}
 
-	dag.transitive_reduce()
+	dag.Transitive_reduce()
 
-	if dag.count_edges() != 5 {
-		t.Errorf("dag has %v edges, expected 5 edges", dag.count_edges())
+	if dag.Count_edges() != 5 {
+		t.Errorf("dag has %v edges, expected 5 edges", dag.Count_edges())
 	}
 }
