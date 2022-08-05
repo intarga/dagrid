b81a39dbcf6dd54968b5b7d2699b0bb5c0d9a070
Ingrid Rebecca Abraham <ingridra@met.no>
2022-08-05 13:58:34 +0200

    add unit test for Check_cycle

--- dagrid_test.go
+++ dagrid_test.go
@@ -27,3 +27,35 @@ func TestTransitiveReduce(t *testing.T) {
 		t.Errorf("dag has %v edges, expected 5 edges", dag.Count_edges())
 	}
 }
+
+func TestCycleCheck(t *testing.T) {
+	good_dag := New_dag()
+
+	a := good_dag.Insert_free_node("a")
+
+	b := good_dag.Insert_child(a, "b")
+	c := good_dag.Insert_child(a, "c")
+
+	d := good_dag.Insert_child(b, "d")
+	good_dag.Add_edge(c, d)
+
+	if !good_dag.Cycle_check() {
+		t.Error("cycle detected in acyclic graph")
+	}
+
+	bad_dag := New_dag()
+
+	e := bad_dag.Insert_free_node("e")
+
+	f := bad_dag.Insert_child(e, "f")
+	g := bad_dag.Insert_child(e, "g")
+
+	h := bad_dag.Insert_child(f, "h")
+	bad_dag.Add_edge(h, g)
+	bad_dag.Add_edge(g, f)
+
+	if bad_dag.Cycle_check() {
+		t.Error("no cycle detected in cyclic graph")
+	}
+
+}
